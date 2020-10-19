f7c023acccfafca37c037f6e2236f9e73377fa91
idelahoz14 <idelahoz14@cuc.edu.co>
2020-10-19 22:47:27 +0000

    fixing betty mistakes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- *
- *
- *
+ * swap - Swap the value of two variables
+ * @x: Integer
+ * @y: Intger
  */
 void swap(int *x, int *y)
 {
@@ -15,18 +15,20 @@ void swap(int *x, int *y)
 }
 
 /**
+ * selection_sort - sorts an array of integers
+ * @array: array unsorted
+ * @size: size of the array
  *
- *
- *
+ * Return: nothing
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i+1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
